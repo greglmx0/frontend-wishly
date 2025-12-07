8ba070e1fa4ab945e80388d2ad3798d19df3b363
greglmx0 <gregoire.lamoureux@epitech.eu>
2025-12-07 22:21:32 +0100

    feat: add CardLoadingPage and WishlyIcon components

---
+++ components/CardLoadingPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="relative rounded-2xl bg-white p-6 text-center shadow-sm transition-shadow duration-300 hover:shadow-lg md:text-left"
+  >
+    <div
+      class="pointer-events-none absolute -top-4 left-1/2 -translate-x-1/2 transform text-6xl opacity-5 md:left-0 md:translate-x-0"
+    ></div>
+
+    <div class="mb-6 inline-flex rounded-2xl bg-gradient-to-br from-purple-100 to-pink-100 p-4">
+      <WishlyIcon :name="props.icon" :size="32" class="text-purple-600"></WishlyIcon>
+    </div>
+
+    <h3 class="mb-3 text-gray-900">{{ props.title }}</h3>
+    <p class="text-gray-600">{{ props.description }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import WishlyIcon from '~/components/WishlyIcon.vue'
+
+/**
+ * @type {Props}
+ * @property {string} title - Title of the step
+ * @property {string} description - Description of the step
+ */
+type Props = {
+  title: string
+  description: string
+  icon: string
+}
+const props: Props = defineProps<{
+  title: string
+  description: string
+  icon: string
+}>()
+</script>
---
+++ components/WishlyIcon.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="felx-row flex items-center gap-2">
+    <Icon :name="props.name" :color="props.color" :size="props.size" class="inline-block" />
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { PropType } from 'vue'
+
+/**
+ * IconProps interface defines the properties for the Icon component.
+ * It includes the icon name, dimensions, viewBox, and color.
+ * @interface WishlyIconProps
+ * @property {string} name - The name of the icon to be displayed.
+ * @property {number|string} [size=24] - The size of the icon, default is 24.
+ * @property {string} [color='currentColor'] - The color of the icon, default is 'currentColor'.
+ */
+type WishlyIconProps = {
+  name: string
+  size?: number | string
+  color?: string
+}
+
+/**
+ * Props definition with strict typing.
+ * @constant {WishlyIconProps}
+ */
+const props: WishlyIconProps = defineProps({
+  name: {
+    type: String as PropType<string>,
+    required: true,
+  },
+  color: {
+    type: String as PropType<string>,
+    default: 'currentColor',
+  },
+  size: {
+    type: [Number, String] as PropType<number | string>,
+    default: 24,
+  },
+})
+</script>
